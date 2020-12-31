f67c7f60dcfdd9b6355dd8620834b1dfa792ace1
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2020-12-31 12:38:22 -0300

    added mep label generator

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,30 @@
 #include "utils.h"
 
+#include <stdio.h>
+
+int mepaLabelCounter = 0;
+
+int countDigits(int number);
+
 char* nextMEPALabel(){
-    // TODO
-    return "";
+    mepaLabelCounter++;
+
+    int digitsCount = countDigits(mepaLabelCounter);
+    char* mepaLabel = malloc(sizeof(char)*(digitsCount+1)); // TODO free this (memory leak!)
+    sprintf(mepaLabel, "L%d", mepaLabelCounter);
+
+    return mepaLabel;
+}
+
+int countDigits(int number) {
+    int count = 0;
+
+    do {
+        count++;
+        number /= 10;
+    } while (number != 0);
+
+    return count;
 }
 
 void initializeSymbolTable() {
